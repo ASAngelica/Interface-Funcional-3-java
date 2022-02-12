80069825982e8654dd604792004b12df8fcc32b6
ASAngelica <asa196549@gmail.com>
2022-02-11 22:46:15 -0300

    Declared lambda expression

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,12 +3,10 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Consumer;
-import java.util.stream.Collector;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
-import util.UpperCaseName;
 
 public class Program {
 	public static void main(String[] args) {
@@ -21,9 +19,12 @@ public class Program {
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
-		                         
-		                                       //Reference methods
-		List<String> names = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+		
+		Function<Product, String> func = p -> p.getName().toUpperCase();
+		
+		
+		//Reference methods
+		List<String> names = list.stream().map(func).collect(Collectors.toList());
 		
 		              //Reference methods
 		names.forEach(System.out::println);
